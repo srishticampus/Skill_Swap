19b670485beeebd36ff3b7d5f7f93ff8ee29358c
Arjun S <arjuns.sics@gmail.com>
2025-05-09 13:25:47 +0530

    refractor: file organization

---
+++ server/models/user_rating.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserRatingSchema = new mongoose.Schema(
+  {
+    rater: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    ratedUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("UserRating", UserRatingSchema);
